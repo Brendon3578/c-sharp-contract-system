495279f703633d40dccb0937d5becef75346cd5f
Brendon Gomes <brendongomes40@gmail.com>
2024-08-12 16:22:21 -0300

    Adicionar arquivos de projeto.

---
+++ ContractSystem/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace ContractSystem.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ ContractSystem/Entities/Enum/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ContractSystem.Entities.Enum
+{
+    enum WorkerLevel : int
+    {
+
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2,
+    }
+}
